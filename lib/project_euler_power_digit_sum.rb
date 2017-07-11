20d4a67c3044a5a7517939ca751a1a327358debc
Rathi <github email address>
2017-07-11 13:26:50 -0400

    got it

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,11 @@
-# code your solution here
+def power_digit_sum(base, expo)
+  num = 1
+  final_num = 0
+  expo.times do
+  num *=   base
+  end
+  num.to_s.split('').each do |num|
+    final_num += num.to_i
+  end
+  return final_num
+end
